fcf33eceb074c43213a45079db4099f706efc1e0
Andreas Kowalenko <andreas.kowalenko@hotmail.com>
2023-11-02 15:53:04 +0100

    RankingFilterRow margin fixed

--- src/features/rankings/RankingsFilterRow.jsx
+++ src/features/rankings/RankingsFilterRow.jsx
@@ -5,7 +5,7 @@ import { PiGameControllerLight } from "react-icons/pi";
 const StyledFilterRow = styled.div`
     display: flex;
     align-items: center;
-    margin: 0.5rem 0 1rem 0;
+    margin: 0 0 1rem 0;
 `;
 
 function RankingsFilterRow() {
