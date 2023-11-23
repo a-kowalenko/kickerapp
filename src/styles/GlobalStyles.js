44e2f489d3366bbdd848afd7fc336a104c62a870
Andreas Kowalenko <andreas.kowalenko@hotmail.com>
2023-11-23 18:06:34 +0100

    Disable text selecting part2

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -384,6 +384,14 @@ select {
 
 button {
   cursor: pointer;
+
+  // Disables selecting of the inner button text
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none;   /* Safari */
+  -khtml-user-select: none;    /* Konqueror HTML */
+  -moz-user-select: none;      /* Old versions of Firefox */
+  -ms-user-select: none;       /* Internet Explorer/Edge */
+  user-select: none;           /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 }
 
 *:disabled {
